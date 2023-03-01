2c61ec30f0f67f3d7bf4c723ef1fbd55b5d85e97
Haniibani <hanna.behmer@gmail.com>
2023-03-01 16:44:31 +0000

    Add termcolor package to set fontcolor in prints

--- run.py
+++ run.py
@@ -2,44 +2,40 @@ from classes.Deck import Deck
 from classes.Players import Players
 from time import sleep
 import sys
+from termcolor import colored
 
 def animate_text(text):
-    """
-    Function to animate the text as a typewriter.
-    """
-    for i in text:
-        print(i ,end="")
-        sys.stdout.flush()
-        sleep(0.1)
+  for i in text:
+    print(i ,end="")
+    sys.stdout.flush()
+    sleep(0.1)
 
 def main():
-    """
-    Run all program functions
-    """
-    pokerhands = []
-    deck = Deck()
-    deck.shuffle()
-    number_of_players = int(input('How many players (1-7)? ',))
+  pokerhands = []
+  deck = Deck()
+  deck.shuffle()
+  number_of_players = int(input(colored(' How many players (1-7)? ', 'light_blue')))
 
-    for index in range(number_of_players):
-        players = Players(deck.deal())
-        pokerhands.append({**players.best_hand(), 'players_nr': index})
+  for index in range(number_of_players):
+      players = Players(deck.deal())
+      pokerhands.append({**players.best_hand(), 'players_nr': index})
 
-    sorted_hands = sorted(
+  sorted_hands = sorted(
     pokerhands,
     key=lambda hand: (-hand['rank'], -hand['highest_rank'])
-    )
+  )
 
-    for players in sorted_hands:
-        print(
+  for players in sorted_hands:
+      print(
         f' players: {players["players_nr"] + 1}: {players["hand_str"]} =',
-        *players["hand"])
-    
-    play_again = input('Wanna play again? y/n: ')
-    if play_again == "y":
-        main()
-    else:
-        animate_text("Thanks for playing")
+        *players["hand"]
+      )
 
-animate_text("Welcome to Hannas Poker!\n")
+  play_again = input(colored('Wanna play again? y/n: ', 'magenta'))
+  if play_again == "y":
+    main()
+  else:
+    animate_text(colored("Thanks for playing", "blue"))
+
+animate_text(colored("Welcome to Hannas Poker!\n", "magenta"))
 main()

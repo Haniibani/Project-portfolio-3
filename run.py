3ae1faecf1e2b662c27a8e5900c64695ca0d4125
Haniibani <hanna.behmer@gmail.com>
2023-03-01 16:30:05 +0000

    Imported classes and constants to run.py for game to work

--- run.py
+++ run.py
@@ -1,18 +1,21 @@
+from classes.Deck import Deck
+from classes.Players import Players
+
 pokerhands = []
 deck = Deck()
 deck.shuffle()
-number_of_players = int(input(' How many players (1-7)? ', 'light_blue'))
+number_of_players = int(input(' How many players (1-7)? '))
 for index in range(number_of_players):
-players = Players(deck.deal())
-pokerhands.append({**players.best_hand(), 'players_nr': index})
+    players = Players(deck.deal())
+    pokerhands.append({**players.best_hand(), 'players_nr': index})
 
 sorted_hands = sorted(
-pokerhands,
-key=lambda hand: (-hand['rank'], -hand['highest_rank'])
+    pokerhands,
+    key=lambda hand: (-hand['rank'], -hand['highest_rank'])
 )
 
 for players in sorted_hands:
-print(
-f' players: {players["players_nr"] + 1}: {players["hand_str"]} =',
-*players["hand"]
-)
+    print(
+    f' players: {players["players_nr"] + 1}: {players["hand_str"]} =',
+    *players["hand"]
+    )

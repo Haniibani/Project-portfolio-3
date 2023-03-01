d3bfca6537e6b5da9c3065c09fd8da63b0980fcd
Haniibani <hanna.behmer@gmail.com>
2023-03-01 16:33:27 +0000

    Restructured run.py with a main function to call

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
 from classes.Deck import Deck
 from classes.Players import Players
 
-pokerhands = []
-deck = Deck()
-deck.shuffle()
-number_of_players = int(input(' How many players (1-7)? '))
-for index in range(number_of_players):
-    players = Players(deck.deal())
-    pokerhands.append({**players.best_hand(), 'players_nr': index})
+def main():
+    """
+    Run all program functions
+    """
+  pokerhands = []
+  deck = Deck()
+  deck.shuffle()
+  number_of_players = int(input(colored(' How many players (1-7)? ', 'light_blue')))
 
-sorted_hands = sorted(
+  for index in range(number_of_players):
+      players = Players(deck.deal())
+      pokerhands.append({**players.best_hand(), 'players_nr': index})
+
+  sorted_hands = sorted(
     pokerhands,
     key=lambda hand: (-hand['rank'], -hand['highest_rank'])
-)
+  )
 
-for players in sorted_hands:
-    print(
-    f' players: {players["players_nr"] + 1}: {players["hand_str"]} =',
-    *players["hand"]
-    )
+  for players in sorted_hands:
+      print(
+        f' players: {players["players_nr"] + 1}: {players["hand_str"]} =',
+        *players["hand"]

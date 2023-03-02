208615747e7adbecfadd3f72a49303bb5727c34b
Haniibani <hanna.behmer@gmail.com>
2023-03-02 12:24:33 +0000

    Add validation to userinput

--- run.py
+++ run.py
@@ -14,7 +14,13 @@ def main():
   pokerhands = []
   deck = Deck()
   deck.shuffle()
-  number_of_players = int(input(colored(' How many players (1-7)? ', 'light_blue')))
+  
+  number_of_players = 0
+  while number_of_players not in range(1, 8):
+    number_of_players = int(input("Please enter a number between 1 and 7: "))
+    if number_of_players not in range(1, 8):
+      print(colored("Not a valid number" , "red"))
+
 
   for index in range(number_of_players):
       players = Players(deck.deal())
@@ -31,11 +37,15 @@ def main():
         *players["hand"]
       )
 
-  play_again = input(colored('Wanna play again? y/n: ', 'magenta'))
-  if play_again == "y":
-    main()
-  else:
-    animate_text(colored("Thanks for playing", "blue"))
+  while True:
+      play_again = input(colored('Wanna play again? y/n: ', 'magenta'))
+      if play_again in ["y", "Y"]:
+          main()
+      elif play_again in ["n", "N"]:
+          animate_text(colored("Thanks for playing", "blue"))
+          sys.exit() # Exit the program if the user enters "n" or "N"
+      else:
+          print(colored("Please answer y or n\n", "blue"))
 
 animate_text(colored("Welcome to Hannas Poker!\n", "magenta"))
 main()
